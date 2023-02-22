6bff4a4cab789bb8c08a37173724508e945a765d
Emanuela <emanuelasindeauxprata@gmail.com>
2023-02-22 17:27:00 -0300

    ok

--- src/pages/janelasDoSono/index.js
+++ src/pages/janelasDoSono/index.js
@@ -1,9 +1,19 @@
+import React from 'react';
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TouchableOpacity, TouchableHighlight, ScrollView } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
+import { List } from 'react-native-paper';
+
 
 export default function Janelas() {
+
+    // Acordeon #######
+    const [expandedList, setExpandedList] = React.useState(true);
+
+    const handlePressList = () => setExpandedList(!expandedList);
+
+    // TabView ########
 
     const [activeTab, setActiveTab] = useState(0);
 
@@ -12,12 +22,12 @@ export default function Janelas() {
     }
 
     const tabs = [
-        "0",
-        "1-2 ",
-        "3-4",
-        "5-6",
-        "7-8",
-        "9-12",
+        "0 meses",
+        "1-2 meses",
+        "3-4 meses",
+        "5-6 meses",
+        "7-8 meses",
+        "9-12 meses",
 
     ];
 
@@ -51,21 +61,57 @@ export default function Janelas() {
 
     const renderContent = () => {
         return (
-            <View style={styles.content}>
-                <Text>
-                    Dorme entre:
-                </Text>
-                <Text>
-                    Quantidade de sonecas por dia:
-                </Text>
-                <Text>
-                    Tempo das sonecas:
-                </Text>
-                <Text>
-                    Tempo acordado entre sonecas:
-                </Text>
-                
-            </View>
+
+            <ScrollView>
+                <View>
+                    <View style={styles.flexRow}>
+                        <Image
+                            style={{ width: 35, height: 35 }} source={require('./assets/berco.png')}
+                        />
+                        <Text>
+                            Dorme entre:
+                        </Text>
+                        <Text>2-3</Text>
+                    </View>
+
+                    <View style={styles.flexRow}>
+                        <Image
+                            style={{ width: 50, height: 50 }} source={require('./assets/sono.png')}
+                        />
+                        <Text>
+                            Quantidade de sonecas por dia:
+                        </Text>
+                        <Text>2</Text>
+                    </View>
+
+                    <View style={styles.flexRow}>
+                        <Image
+                            style={{ width: 35, height: 35 }} source={require('./assets/relogio.png')}
+                        />
+                        <Text>
+                            Tempo das sonecas:
+                        </Text>
+                        <Text>2-3</Text>
+                    </View>
+
+                    <View style={styles.flexRow}>
+                        <Image
+                            style={{ width: 35, height: 35 }} source={require('./assets/acordado.png')}
+                        />
+                        <Text>
+                            Tempo acordado entre sonecas:
+                        </Text>
+                        <Text>2</Text>
+                    </View>
+
+
+                </View>
+
+
+            </ScrollView>
+
+
+
         );
     };
 
@@ -86,17 +132,27 @@ export default function Janelas() {
                     style={styles.icon} />
             </View>
 
-            <View>
-                <Text>
-                    {renderTabs()}
+            <List.Accordion
+                title="O que é?"
+                style={styles.itemAcordion}
+                onPress={handlePress}>
+                <Text style={styles.textInfo}>Diz respeito ao crescimento físico. O crescimento não é constante, ele ocorre de forma mais intensa em alguns períodos específicos que chamamos de picos.</Text>
+            </List.Accordion>
+
+            <ScrollView
+                horizontal={true}
+                horizontal showsHorizontalScrollIndicator={false}
+                >
+                <View>
+
+                    <Text>
+                        {renderTabs()}
                     </Text>
-                    <Text style={{textAlign: 'center', fontSize: 12}}>meses</Text>
-            </View>
+                </View>
+            </ScrollView>
 
             <ScrollView>
-                <View>
-                    {renderContent()}
-                </View>
+                {renderContent()}
             </ScrollView>
         </View>
     )
@@ -108,7 +164,7 @@ const styles = StyleSheet.create({
     },
 
     flexRow: {
-        display: 'flex', flexDirection: 'row', flexWrap: 'wrap'
+        display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'flex-start', marginHorizontal: 20
     },
 
     textTitle: {
@@ -144,8 +200,20 @@ const styles = StyleSheet.create({
     },
     content: {
         flex: 1,
-        alignItems: "center",
         justifyContent: "center",
+    },
+
+    textInfo: {
+        fontSize: 15,
+        color: '#573205',
+        width: '100%',
+        textAlign: 'center'
+    },
+
+    itemAcordion: {
+        backgroundColor: '#DDAE76',
+        marginHorizontal: 20,
+        marginVertical: 10
     },
 });
 
